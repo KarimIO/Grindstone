09cb5f7e4fdd32291bd9cdb56953183a78ee918d
KarimIO <karimah@aucegypt.edu>
2021-03-21 00:10:20 +0200

    Hotfix >> Re-add run call in ApplicationExecutable Runtime

--- sources/code/ApplicationExecutable/Main.cpp
+++ sources/code/ApplicationExecutable/Main.cpp
@@ -27,6 +27,7 @@ int main() {
 	create_info.applicationModuleName = "ApplicationDLL";
 	create_info.applicationTitle = "Grindstone Sandbox";
 	EngineCore* engineCore = createEngineFn(create_info);
+	engineCore->run();
 	
 	Grindstone::Utilities::Modules::unload(handle);
 	return 0;
