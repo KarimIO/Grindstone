c8959285ff501df34366b1a1fda4a3115cc14d10
Karim Abdel Hamid <karimah@aucegypt.edu>
2023-06-27 01:00:38 +0300

    Control Bar / Add Gizmo and Play icons

--- sources/code/Editor/ImguiEditor/ControlBar.cpp
+++ sources/code/Editor/ImguiEditor/ControlBar.cpp
@@ -1,39 +1,43 @@
 #include <imgui.h>
 #include <imgui_internal.h>
 #include "ControlBar.hpp"
-#include "Editor/EditorManager.hpp"
+#include "EngineCore/Assets/AssetManager.hpp"
+#include "EngineCore/Assets/Textures/TextureAsset.hpp"
+#include "Plugins/GraphicsOpenGL/GLTexture.hpp"
+
 using namespace Grindstone;
 using namespace Grindstone::Editor;
 using namespace Grindstone::Editor::ImguiEditor;
+
+ControlBar::ControlBar() {
+	pauseIcon = GetTexture("PauseButton.dds");
+	playIcon = GetTexture("PlayButton.dds");
+	translateIcon = GetTexture("Translate.dds");
+	rotateIcon = GetTexture("Rotate.dds");
+	scaleIcon = GetTexture("Scale.dds");
+}
 
 void ControlBar::Render() {
 	ImGuiWindowFlags flags = ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoScrollWithMouse;
 	if (ImGui::Begin("ControlBar", nullptr, flags)) {
 		Editor::Manager& editorManager = Grindstone::Editor::Manager::GetInstance();
 
-		PlayMode playMode = editorManager.GetPlayMode();
-		bool isPlayMode = playMode == PlayMode::Play;
-		const char* playModeText = isPlayMode ? "Quit Play Mode" : "Enter Play Mode";
-		if (ImGui::Button(playModeText)) {
-			PlayMode newMode = isPlayMode ? PlayMode::Editor : PlayMode::Play;
-			editorManager.SetPlayMode(newMode);
-		}
-		ImGui::SameLine();
+		float centerX = ImGui::GetWindowContentRegionWidth() / 2.0f - 12.0f;
+
+		selectedColor = ImGui::GetStyleColorVec4(ImGuiCol_Button);
+		selectedHighlightColor = ImGui::GetStyleColorVec4(ImGuiCol_ButtonHovered);
+		selectedActiveColor = ImGui::GetStyleColorVec4(ImGuiCol_ButtonActive);
+
+		deselectedColor = ImVec4(1.f, 1.f, 1.f, 0.1f);
+		deselectedHighlightColor = ImVec4(1.f, 1.f, 1.f, 0.2f);
+		deselectedActiveColor = ImVec4(1.f, 1.f, 1.f, 0.3f);
 
 		ManipulationMode& manipulationMode = editorManager.manipulationMode;
-		if (ImGui::RadioButton("Translate", manipulationMode == ManipulationMode::Translate)) {
-			manipulationMode = ManipulationMode::Translate;
-		}
+		RenderManipulationButton(translateIcon, manipulationMode, ManipulationMode::Translate);
 		ImGui::SameLine();
-
-		if (ImGui::RadioButton("Rotate", manipulationMode == ManipulationMode::Rotate)) {
-			manipulationMode = ManipulationMode::Rotate;
-		}
+		RenderManipulationButton(rotateIcon, manipulationMode, ManipulationMode::Rotate);
 		ImGui::SameLine();
-
-		if (ImGui::RadioButton("Scale", manipulationMode == ManipulationMode::Scale)) {
-			manipulationMode = ManipulationMode::Scale;
-		}
+		RenderManipulationButton(scaleIcon, manipulationMode, ManipulationMode::Scale);
 		ImGui::SameLine();
 
 		bool& isManipulatingInWorldSpace = editorManager.isManipulatingInWorldSpace;
@@ -42,6 +46,48 @@ void ControlBar::Render() {
 			isManipulatingInWorldSpace = !isManipulatingInWorldSpace;
 		}
 
+		ImGui::SameLine();
+
+		ImGui::SetCursorPosX(centerX);
+		PlayMode playMode = editorManager.GetPlayMode();
+		bool isPlayMode = playMode == PlayMode::Play;
+		const char* playModeText = isPlayMode ? "Quit Play Mode" : "Enter Play Mode";
+		if (RenderButton(playIcon, isPlayMode)) {
+			PlayMode newMode = isPlayMode ? PlayMode::Editor : PlayMode::Play;
+			editorManager.SetPlayMode(newMode);
+		}
+
 		ImGui::End();
 	}
+}
+
+bool ControlBar::RenderButton(ImTextureID icon, bool isSelected) {
+	ImGui::PushStyleColor(ImGuiCol_Button, isSelected ? selectedColor : deselectedColor);
+	ImGui::PushStyleColor(ImGuiCol_ButtonActive, isSelected ? selectedActiveColor : deselectedActiveColor);
+	ImGui::PushStyleColor(ImGuiCol_ButtonHovered, isSelected ? selectedHighlightColor : deselectedHighlightColor);
+
+	int isPressed = ImGui::ImageButton(icon, ImVec2(16, 16), ImVec2(0, 0), ImVec2(1, 1), 4, ImVec4(0.0f, 0.0f, 0.0f, 0.0f), ImVec4(1.f, 1.f, 1.f, 1.f));
+
+	ImGui::PopStyleColor(3);
+
+	return isPressed;
+}
+
+void ControlBar::RenderManipulationButton(ImTextureID icon, ManipulationMode& selectedMode, ManipulationMode buttonMode) {
+	if (RenderButton(icon, buttonMode == selectedMode)) {
+		selectedMode = buttonMode;
+	}
+}
+
+ImTextureID ControlBar::GetTexture(std::string fileName) {
+	std::string path = std::string("../engineassets/editor/controlbarIcons/") + fileName;
+	auto assetManager = Editor::Manager::GetEngineCore().assetManager;
+	auto textureAsset = static_cast<TextureAsset*>(assetManager->GetAsset(Grindstone::AssetType::Texture, path.c_str()));
+
+	if (textureAsset == nullptr) {
+		return 0;
+	}
+
+	GraphicsAPI::GLTexture* glTex = (GraphicsAPI::GLTexture*)textureAsset->texture;
+	return (ImTextureID)(uint64_t)glTex->GetTexture();
 }
--- sources/code/Editor/ImguiEditor/ControlBar.hpp
+++ sources/code/Editor/ImguiEditor/ControlBar.hpp
@@ -1,11 +1,31 @@
 #pragma once
 
+#include "Editor/EditorManager.hpp"
+
 namespace Grindstone {
 	namespace Editor {
 		namespace ImguiEditor {
 			class ControlBar {
 			public:
+				ControlBar();
 				void Render();
+			private:
+				bool RenderButton(ImTextureID icon, bool isSelected);
+				void RenderManipulationButton(ImTextureID icon, ManipulationMode& selectedMode, ManipulationMode buttonMode);
+				ImTextureID GetTexture(std::string fileName);
+
+				ImVec4 selectedColor;
+				ImVec4 deselectedColor;
+				ImVec4 selectedHighlightColor;
+				ImVec4 deselectedHighlightColor;
+				ImVec4 selectedActiveColor;
+				ImVec4 deselectedActiveColor;
+
+				ImTextureID pauseIcon;
+				ImTextureID playIcon;
+				ImTextureID translateIcon;
+				ImTextureID rotateIcon;
+				ImTextureID scaleIcon;
 			};
 		}
 	}
