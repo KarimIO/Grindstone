ce5d3164ec6fd9f4f101a5afe92a6bab97339518
KarimIO <karimah@aucegypt.edu>
2023-07-19 15:53:30 +0300

    C# / EulerAngles / fmod 2pi

--- sources/code/CSharpInterfaces/GrindstoneCore/Math/EulerAngles.cs
+++ sources/code/CSharpInterfaces/GrindstoneCore/Math/EulerAngles.cs
@@ -10,18 +10,25 @@ namespace Grindstone {
 			public float roll;
 			#endregion
 
+			#region Constants
+			const float TWO_PI = (float)(2 * System.Math.PI);
+			#endregion
+
 			#region Constructor Methods
 			public EulerAngles(float pitch, float yaw, float roll) {
-				this.pitch = pitch;
-				this.yaw = yaw;
-				this.roll = roll;
+				this.pitch = pitch % TWO_PI;
+				this.yaw = yaw % TWO_PI;
+				this.roll = roll % TWO_PI;
 			}
 
 			public EulerAngles(Float3 eulerAngles) {
-				pitch = eulerAngles.x;
-				yaw = eulerAngles.y;
-				roll = eulerAngles.z;
+				pitch = eulerAngles.x % TWO_PI;
+				yaw = eulerAngles.y % TWO_PI;
+				roll = eulerAngles.z % TWO_PI;
 			}
+
+
+			public static EulerAngles Zero => new EulerAngles(0.0f, 0.0f, 0.0f);
 			#endregion
 
 			#region Public Methods
