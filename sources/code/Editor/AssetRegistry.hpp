b3e84f9e849b2d2c69ab1db2ddfa2be6af3a527b
KarimIO <karimah@aucegypt.edu>
2023-07-24 15:29:07 +0300

    Add AssetRegistry

---
+++ sources/code/Editor/AssetRegistry.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <map>
+#include <filesystem>
+
+#include <Common/ResourcePipeline/Uuid.hpp>
+#include <Common/ResourcePipeline/AssetType.hpp>
+
+namespace Grindstone::Editor {
+	class AssetRegistry {
+	public:
+		struct Entry {
+			Uuid uuid;
+			std::string name;
+			std::filesystem::path path;
+			AssetType assetType;
+		};
+	public:
+		void Initialize(const std::filesystem::path& projectPath);
+		void Cleanup();
+		void UpdateEntry(std::filesystem::path& path, std::string& name, Uuid& uuid, AssetType assetType);
+		void WriteFile();
+		void ReadFile();
+
+		bool HasAsset(Uuid uuid);
+		bool TryGetAssetData(Uuid uuid, AssetRegistry::Entry& outEntry);
+	private:
+		std::map<Uuid, Entry> assets;
+		std::filesystem::path assetsPath;
+		std::filesystem::path assetRegistryPath;
+	};
+}
