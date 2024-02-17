c7da8b345e66d56bd3efcf7d6d5665100eb7b3db
Karim Abdel Hamid <karimah@aucegypt.edu>
2024-02-16 21:23:01 -0600

    Fix glm warnings in export

--- sources/code/EngineCore/CoreComponents/Transform/TransformComponent.cpp
+++ sources/code/EngineCore/CoreComponents/Transform/TransformComponent.cpp
@@ -3,46 +3,76 @@
 #include "EngineCore/Scenes/Scene.hpp"
 using namespace Grindstone;
 
+ENGINE_CORE_API typedef struct ExportableVector {
+	float x;
+	float y;
+	float z;
+} ExportableVector;
+
+ENGINE_CORE_API typedef struct ExportableQuaternion {
+	float x;
+	float y;
+	float z;
+	float w;
+} ExportableQuaternion;
+
+glm::vec3 ImportVector(const ExportableVector inVec) {
+	return { inVec.x, inVec.y, inVec.z };
+}
+
+glm::quat ImportQuaternion(const ExportableQuaternion inQuat) {
+	return { inQuat.x, inQuat.y, inQuat.z, inQuat.w };
+}
+
 extern "C" {
+	ExportableVector ExportVector(const glm::vec3 inVec) {
+		return { inVec.x, inVec.y, inVec.z };
+	}
+
+	ExportableQuaternion ExportQuaternion(const glm::quat inQuat) {
+		return { inQuat.x, inQuat.y, inQuat.z, inQuat.w };
+	}
+
 	ENGINE_CORE_API void* EntityGetTransformComponent(Grindstone::SceneManagement::Scene* scene, uint32_t entity) {
 		entt::registry& reg = scene->GetEntityRegistry();
-		return &reg.get<TransformComponent>((entt::entity)entity);
+		const entt::entity entityId = static_cast<entt::entity>(entity);
+		return &reg.get<TransformComponent>(entityId);
 	}
 
-	ENGINE_CORE_API Math::Quaternion TransformComponentGetRotation(TransformComponent& component) {
-		return component.rotation;
+	ENGINE_CORE_API ExportableQuaternion TransformComponentGetRotation(const TransformComponent& component) {
+		return ExportQuaternion(component.rotation);
 	}
 
-	ENGINE_CORE_API void TransformComponentSetRotation(TransformComponent& component, Math::Quaternion rotation) {
-		component.rotation = rotation;
+	ENGINE_CORE_API void TransformComponentSetRotation(TransformComponent& component, ExportableQuaternion rotation) {
+		component.rotation = ImportQuaternion(rotation);
 	}
 
-	ENGINE_CORE_API Math::Float3 TransformComponentGetPosition(TransformComponent& component) {
-		return component.position;
+	ENGINE_CORE_API ExportableVector TransformComponentGetPosition(const TransformComponent& component) {
+		return ExportVector(component.position);
 	}
 
-	ENGINE_CORE_API void TransformComponentSetPosition(TransformComponent& component, Math::Float3 position) {
-		component.position = position;
+	ENGINE_CORE_API void TransformComponentSetPosition(TransformComponent& component, ExportableVector position) {
+		component.position = ImportVector(position);
 	}
 
-	ENGINE_CORE_API Math::Float3 TransformComponentGetScale(TransformComponent& component) {
-		return component.scale;
+	ENGINE_CORE_API ExportableVector TransformComponentGetScale(const TransformComponent& component) {
+		return ExportVector(component.scale);
 	}
 
-	ENGINE_CORE_API void TransformComponentSetScale(TransformComponent& component, Math::Float3 scale) {
-		component.scale = scale;
+	ENGINE_CORE_API void TransformComponentSetScale(TransformComponent& component, const ExportableVector scale) {
+		component.scale = ImportVector(scale);
 	}
 
-	ENGINE_CORE_API Math::Float3 TransformComponentGetForward(TransformComponent& component) {
-		return component.GetForward();
+	ENGINE_CORE_API ExportableVector TransformComponentGetForward(const TransformComponent& component) {
+		return ExportVector(component.GetForward());
 	}
 
-	ENGINE_CORE_API Math::Float3 TransformComponentGetRight(TransformComponent& component) {
-		return component.GetRight();
+	ENGINE_CORE_API ExportableVector TransformComponentGetRight(const TransformComponent& component) {
+		return ExportVector(component.GetRight());
 	}
 
-	ENGINE_CORE_API Math::Float3 TransformComponentGetUp(TransformComponent& component) {
-		return component.GetUp();
+	ENGINE_CORE_API ExportableVector TransformComponentGetUp(const TransformComponent& component) {
+		return ExportVector(component.GetUp());
 	}
 }
 
