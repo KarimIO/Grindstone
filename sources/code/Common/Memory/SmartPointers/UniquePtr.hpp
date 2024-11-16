cfb648831ff94e15d9521d8b1b4507118b3a223b
Karim Abdel Hamid <karimah@aucegypt.edu>
2024-11-16 04:33:33 -0500

    Implement UniquePtr more thoroughly

--- sources/code/Common/Memory/SmartPointers/UniquePtr.hpp
+++ sources/code/Common/Memory/SmartPointers/UniquePtr.hpp
@@ -9,20 +9,58 @@ namespace Grindstone::Memory::SmartPointers {
 
 		UniquePtr() = default;
 
-		UniquePtr(T* ptr, std::function<void(void*)> deleteFn) : ptr(ptr), deleteFn(deleteFn) {}
+		explicit UniquePtr(T* ptr, std::function<void(void*)> deleteFn) : ptr(ptr), deleteFn(deleteFn) {}
+		UniquePtr(std::nullptr_t) : ptr(nullptr), deleteFn(nullptr) {}
 
-		UniquePtr(const UniquePtr& obj) = delete;
-		UniquePtr& operator=(const UniquePtr& obj) = delete;
+		UniquePtr(const UniquePtr<T>& other) = delete;
+		UniquePtr& operator=(const UniquePtr<T>& other) = delete;
 
-		T* operator->() {
-			return this->ptr;
+		UniquePtr& operator=(std::nullptr_t) {
+			Release();
+			return *this;
 		}
 
-		T& operator*() {
-			return *(this->ptr);
+		UniquePtr(UniquePtr<T>&& other) noexcept : ptr(nullptr), deleteFn(nullptr) {
+			std::swap(other.ptr, ptr);
+			std::swap(other.deleteFn, deleteFn);
 		}
 
-		~UniquePtr() {
+		UniquePtr<T>& operator=(UniquePtr<T>&& other) noexcept {
+			if (other.ptr != nullptr) {
+				Release();
+			}
+
+			ptr = other.ptr;
+			deleteFn = other.deleteFn;
+
+			other.Release();
+
+			return *this;
+		}
+
+		template<typename U>
+		UniquePtr(UniquePtr<U>&& other) {
+			UniquePtr<T> tmp(other.Release());
+			tmp.swap(*this);
+		}
+
+		template<typename U>
+		UniquePtr& operator=(UniquePtr<U>&& other) {
+			auto deleteFn = other.GetDeleter();
+			UniquePtr<T> tmp(other.Release(), deleteFn);
+			tmp.Swap(*this);
+			return *this;
+		}
+
+		T* Release() noexcept {
+			T* emptyPtr = nullptr;
+			std::swap(emptyPtr, ptr);
+			deleteFn = nullptr;
+
+			return emptyPtr;
+		}
+
+		void Reset() noexcept {
 			if (ptr != nullptr) {
 				ptr->~T();
 
@@ -32,6 +70,39 @@ namespace Grindstone::Memory::SmartPointers {
 
 				ptr = nullptr;
 			}
+		}
+
+		void Swap(UniquePtr& other) noexcept {
+			std::swap(ptr, other.ptr);
+			std::swap(deleteFn, other.deleteFn);
+		}
+
+		explicit operator T*() const {
+			return ptr;
+		}
+
+		explicit operator bool() const {
+			return ptr != nullptr;
+		}
+
+		T* operator->() const {
+			return this->ptr;
+		}
+
+		T& operator*() const {
+			return *(this->ptr);
+		}
+
+		T* Get() const {
+			return this->ptr;
+		}
+
+		std::function<void(void*)> GetDeleter() const {
+			return this->deleteFn;
+		}
+
+		~UniquePtr() {
+			Reset();
 		}
 
 	private:
