63194a7d1e8a32e6d2d0af65400e837c9f0cf19e
Karim Abdel Hamid <karimah@aucegypt.edu>
2025-05-31 02:48:37 -0400

    Set first pipelineset as default

--- sources/code/Plugins/EditorPipelineSetImporter/PipelineSetImporter.cpp
+++ sources/code/Plugins/EditorPipelineSetImporter/PipelineSetImporter.cpp
@@ -46,14 +46,17 @@ void Grindstone::Editor::Importers::ImportShadersFromGlsl(Grindstone::Editor::As
 	WriteCallback writeCallback = [&assetRegistry, &assetManager] (const std::filesystem::path& path, const std::vector<PipelineOutput>& pipelines) {
 		Grindstone::Editor::MetaFile* metaFile = assetRegistry.GetMetaFileByPath(path);
 
-		for (const PipelineOutput& pout : pipelines) {
+		for (size_t index = 0; index < pipelines.size(); ++index) {
+			const PipelineOutput& pout = pipelines[index];
 			Grindstone::AssetType assetType =
 				pout.pipelineType == Grindstone::Formats::Pipelines::V1::PipelineType::Graphics
 				? Grindstone::AssetType::GraphicsPipelineSet
 				: Grindstone::AssetType::ComputePipelineSet;
 
 			std::string pipelineName = std::string(pout.name);
-			Grindstone::Uuid uuid = metaFile->GetOrCreateSubassetUuid(pipelineName, assetType);
+			Grindstone::Uuid uuid = index == 0
+				? metaFile->GetOrCreateDefaultSubassetUuid(pipelineName, assetType)
+				: metaFile->GetOrCreateSubassetUuid(pipelineName, assetType);
 			std::filesystem::path outputPath = assetRegistry.GetCompiledAssetsPath() / uuid.ToString();
 
 			std::ofstream outStream(outputPath, std::ios::binary);
