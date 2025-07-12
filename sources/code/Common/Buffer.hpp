e02c79272bbb19be541355fdc1bab74b1ea2ddad
Karim Abdel Hamid <karimah@aucegypt.edu>
2025-07-12 18:21:12 -0400

    Buffer / Fix the constructors being used in a non-owning way by creating static functions

--- sources/code/Common/Buffer.hpp
+++ sources/code/Common/Buffer.hpp
@@ -9,14 +9,12 @@ namespace Grindstone {
 	class Buffer {
 	public:
 		Buffer() : bufferPtr(nullptr), capacity(0) {}
-		Buffer(uint64_t capacity) : capacity(capacity) {
+		Buffer(uint64_t newCapacity) : capacity(newCapacity) {
 			this->bufferPtr = static_cast<Byte*>(Grindstone::Memory::AllocatorCore::AllocateRaw(capacity, alignof(Buffer), "Buffer"));
 		}
 
-		Buffer(void* bufferPtr, const uint64_t capacity) : bufferPtr(static_cast<Byte*>(bufferPtr)), capacity(capacity) {}
-
 		// Copy-Constructor
-		Buffer(const Buffer& other) : capacity(other.capacity) {
+		Buffer(const Buffer& other) : bufferPtr(nullptr), capacity(other.capacity) {
 			bufferPtr = static_cast<Byte*>(Grindstone::Memory::AllocatorCore::AllocateRaw(capacity, alignof(Buffer), "Buffer"));
 			memcpy(bufferPtr, other.bufferPtr, capacity);
 		}
@@ -41,7 +39,7 @@ namespace Grindstone {
 			}
 		}
 
-		virtual Grindstone::Containers::BufferSpan GetSpan(uint64_t segmentOffset, uint64_t segmentSize) {
+		[[nodiscard]] virtual Grindstone::Containers::BufferSpan GetSpan(uint64_t segmentOffset, uint64_t segmentSize) {
 			Byte* targetPtr = bufferPtr + segmentOffset;
 			if (targetPtr < bufferPtr) {
 				GS_ASSERT_ENGINE("Start of span is before start of buffer.");
@@ -57,7 +55,7 @@ namespace Grindstone {
 		}
 
 		template<typename T>
-		Grindstone::Containers::Span<T> GetSpan(uint64_t offset, uint64_t count) {
+		[[nodiscard]] Grindstone::Containers::Span<T> GetSpan(uint64_t offset, uint64_t count) {
 			Byte* bytePtr = bufferPtr + offset;
 			T* targetPtr = reinterpret_cast<T*>(bytePtr);
 			if (bytePtr < bufferPtr) {
@@ -74,7 +72,7 @@ namespace Grindstone {
 		}
 
 		template<typename T>
-		T* Get(uint64_t offset) {
+		[[nodiscard]] T* Get(uint64_t offset) {
 			Byte* bytePtr = bufferPtr + offset;
 			T* targetPtr = reinterpret_cast<T*>(bytePtr);
 			if (bytePtr < bufferPtr) {
@@ -91,7 +89,7 @@ namespace Grindstone {
 		}
 
 		template<typename T>
-		const T* Get(uint64_t offset) const {
+		[[nodiscard]] const T* Get(uint64_t offset) const {
 			Byte* bytePtr = bufferPtr + offset;
 			T* targetPtr = reinterpret_cast<T*>(bytePtr);
 			if (bytePtr < bufferPtr) {
@@ -105,6 +103,36 @@ namespace Grindstone {
 			}
 
 			return targetPtr;
+		}
+
+		[[nodiscard]] Byte* Get(uint64_t offset) {
+			Byte* bytePtr = bufferPtr + offset;
+			if (bytePtr < bufferPtr) {
+				GS_ASSERT_ENGINE("Offset is before start of buffer.");
+				return nullptr;
+			}
+
+			if (reinterpret_cast<Byte*>(bytePtr) >= bufferPtr + capacity) {
+				GS_ASSERT_ENGINE("Offset is at or after end of buffer.");
+				return nullptr;
+			}
+
+			return bytePtr;
+		}
+
+		[[nodiscard]] Byte* Get(uint64_t offset) const {
+			Byte* bytePtr = bufferPtr + offset;
+			if (bytePtr < bufferPtr) {
+				GS_ASSERT_ENGINE("Offset is before start of buffer.");
+				return nullptr;
+			}
+
+			if (reinterpret_cast<Byte*>(bytePtr) >= bufferPtr + capacity) {
+				GS_ASSERT_ENGINE("Offset is at or after end of buffer.");
+				return nullptr;
+			}
+
+			return bytePtr;
 		}
 
 		Buffer& operator=(const Buffer& other) {
@@ -136,11 +164,11 @@ namespace Grindstone {
 			return *this;
 		}
 
-		Byte& operator[](int index) {
+		[[nodiscard]] Byte& operator[](int index) {
 			return bufferPtr[index];
 		}
 
-		Byte operator[](int index) const {
+		[[nodiscard]] Byte operator[](int index) const {
 			return bufferPtr[index];
 		}
 
@@ -157,19 +185,32 @@ namespace Grindstone {
 			capacity = 0;
 		}
 
-		Byte* Get() {
+		[[nodiscard]] Byte* Get() {
 			return bufferPtr;
 		}
 
-		const Byte* Get() const {
+		[[nodiscard]] const Byte* Get() const {
 			return bufferPtr;
 		}
 
-		uint64_t GetCapacity() const {
+		[[nodiscard]] uint64_t GetCapacity() const {
 			return capacity;
 		}
 
+		[[nodiscard]] static Buffer MakeCopiedBuffer(void* srcBufferPtr, const uint64_t capacity) {
+			Byte* bufferPtr = static_cast<Byte*>(Grindstone::Memory::AllocatorCore::AllocateRaw(capacity, alignof(Buffer), "Buffer"));
+			memcpy(bufferPtr, srcBufferPtr, capacity);
+			return Buffer( bufferPtr, capacity );
+		}
+
+		[[nodiscard]] static Buffer MakeMovedBuffer(void* srcBufferPtr, const uint64_t capacity) {
+			Byte* bufferPtr = static_cast<Byte*>(srcBufferPtr);
+			return Buffer( bufferPtr, capacity );
+		}
+
 	protected:
+		Buffer(void* bufferPtr, const uint64_t capacity) : bufferPtr(static_cast<Byte*>(bufferPtr)), capacity(capacity) {}
+
 		Byte* bufferPtr = nullptr;
 		uint64_t capacity = 0;
 	};
