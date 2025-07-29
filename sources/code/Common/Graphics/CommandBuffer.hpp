e82d062391785928a106fe7a2fca811fd3954bf7
Karim Abdel Hamid <karimah@aucegypt.edu>
2025-07-29 01:01:28 -0500

    RHI / Consolidate PipelineBarrier commands

--- sources/code/Common/Graphics/CommandBuffer.hpp
+++ sources/code/Common/Graphics/CommandBuffer.hpp
@@ -13,32 +13,57 @@ namespace Grindstone::GraphicsAPI {
 	class VertexArrayObject;
 	class DepthStencilTarget;
 
-	enum class PipelineStage : uint8_t {
-		TopOfPipe,
-		FragmentShader,
-		ComputeShader,
-		ColorAttachmentOutput,
-		BottomOfPipe,
-	};
-
-	enum class AccessFlags : uint8_t {
+	enum class AccessFlags : uint32_t {
 		None = 0,
-		Read = 1 << 0,
-		Write = 1 << 1,
+		IndirectCommandRead = 0x00000001,
+		IndexRead = 0x00000002,
+		VertexAttributeRead = 0x00000004,
+		UNIFORMRead = 0x00000008,
+		InputAttachmentRead = 0x00000010,
+		ShaderRead = 0x00000020,
+		ShaderWrite = 0x00000040,
+		ColorAttachmentRead = 0x00000080,
+		ColorAttachmentWrite = 0x00000100,
+		DepthStencilAttachmentRead = 0x00000200,
+		DepthStencilAttachmentWrite = 0x00000400,
+		TransferRead = 0x00000800,
+		TransferWrite = 0x00001000,
+		HostRead = 0x00002000,
+		HostWrite = 0x00004000,
+		MemoryRead = 0x00008000,
+		MemoryWrite = 0x00010000,
+		TransformFeedbackWrite = 0x02000000,
+		TransformFeedbackCounterRead = 0x04000000,
+		TransformFeedbackCounterWrite = 0x08000000,
+		ConditionalRenderingRead = 0x00100000,
+		ColorAttachmentReadNoncoherent = 0x00080000,
+		AccelerationStructureRead = 0x00200000,
+		AccelerationStructureWrite = 0x00400000,
+		FragmentDensityMapRead = 0x01000000,
+		FragmentShadingRateAttachmentRead = 0x00800000,
+		CommandPreprocessRead = 0x00020000,
+		CommandPreprocessWrite = 0x00040000
 	};
 
 	struct ImageBarrier {
-		GraphicsAPI::Image* image;
+		GraphicsAPI::Image* image = nullptr;
 		ImageLayout oldLayout;
 		ImageLayout newLayout;
-		PipelineStage srcStage;
-		PipelineStage dstStage;
 		AccessFlags srcAccess;
 		AccessFlags dstAccess;
+		ImageAspectBits imageAspect;
 		uint32_t baseMipLevel = 0;
 		uint32_t levelCount = 0;
 		uint32_t baseArrayLayer = 0;
 		uint32_t layerCount = 0;
+	};
+
+	struct BufferBarrier {
+		GraphicsAPI::Buffer* buffer = nullptr;
+		AccessFlags srcAccess;
+		AccessFlags dstAccess;
+		uint32_t offset = 0;
+		uint32_t size = 0;
 	};
 
 	/*! CommandBuffers are an object that hold a list of commands to be executed
@@ -93,8 +118,11 @@ namespace Grindstone::GraphicsAPI {
 			uint32_t width, uint32_t height, uint32_t depth
 		) = 0;
 
-		virtual void WaitForComputeMemoryBarrier(Image* renderTarget, bool shouldMakeWritable) = 0;
-		virtual void PipelineBarrier(const GraphicsAPI::ImageBarrier* barriers, uint32_t barrierCount) = 0;
+		virtual void PipelineBarrier(
+			GraphicsAPI::PipelineStageBit srcPipelineStageMask, GraphicsAPI::PipelineStageBit dstPipelineStageMask,
+			const GraphicsAPI::BufferBarrier* bufferBarriers, uint32_t bufferBarrierCount,
+			const GraphicsAPI::ImageBarrier* imageBarriers, uint32_t imageBarrierCount
+		) = 0;
 
 		virtual void EndCommandBuffer() = 0;
 
