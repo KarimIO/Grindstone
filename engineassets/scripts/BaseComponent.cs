000e894f50a40fd2f71e346ae01b08a2cd40aab5
Karim Abdel Hamid <karimah@aucegypt.edu>
2025-09-09 23:57:48 -0400

    Plugins / Editor / Move images to Editor assets plugin and RendererDeferred

--- engineassets/scripts/BaseComponent.cs
+++
@@ -1,7 +0,0 @@
-using System;
-
-public abstract class BaseComponent {
-    public virtual void initialize()    { }
-    public virtual void start()         { }
-    public virtual void update()        { }
-}
---
+++ plugins/Grindstone.Editor.Assets/README.md
@@ -0,0 +1,3 @@
+# Editor Assets
+
+This plugin is developed by the Grindstone Foundation. This includes icons and fonts used by the editor UI.
