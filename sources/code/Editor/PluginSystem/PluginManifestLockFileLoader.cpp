4509172f5fb64d6e52cfcc179cd127bc36da93e9
Karim Abdel Hamid <karimah@aucegypt.edu>
2025-10-16 18:26:07 -0400

    Plugins / Remove plugins that don't exist, and call error

--- sources/code/Editor/PluginSystem/PluginManifestLockFileLoader.cpp
+++ sources/code/Editor/PluginSystem/PluginManifestLockFileLoader.cpp
@@ -8,13 +8,19 @@ bool Grindstone::Plugins::LoadPluginManifestLockFile(std::vector<ManifestData>& 
 	auto pluginManager = static_cast<Grindstone::Plugins::EditorPluginManager*>(Grindstone::EngineCore::GetInstance().GetPluginManager());
 	const std::vector<std::filesystem::path> &pluginsFolders = pluginManager->GetPluginsFolders();
 
-	for (ManifestData& manifestData : manifestDataList) {
+	for (int i = manifestDataList.size() - 1; i >= 0; --i) {
+		ManifestData& manifestData = manifestDataList[i];
 		for (const std::filesystem::path& basePluginPath : pluginsFolders) {
 			std::filesystem::path metaFilePath = basePluginPath / manifestData.pluginName;
 			if (std::filesystem::exists(metaFilePath / "plugin.meta.json")) {
 				manifestData.path = metaFilePath;
 				break;
 			}
+		}
+
+		if (manifestData.path.empty()) {
+			GPRINT_ERROR_V(Grindstone::LogSource::Editor, "Couldn't find plugin '{}' after searching plugin folders.", manifestData.pluginName.c_str());
+			manifestDataList.erase(manifestDataList.begin() + i);
 		}
 	}
 
