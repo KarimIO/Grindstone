fb50285eb2ca8b073cbf407da1ddecb63ed4cf95
Karim Abdel Hamid <karimah@aucegypt.edu>
2026-05-21 00:07:41 -0400

    Setup Win / Use C:\vcpkg, request admin privs

--- scripts/setup-win.bat
+++ scripts/setup-win.bat
@@ -2,6 +2,34 @@
 
 setlocal enableextensions enabledelayedexpansion
 
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+	>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+	>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+
 net session >nul 2>&1
 if %errorLevel% NEQ 0 (
 	echo Failure: Current permissions inadequate - retry as administrator.
@@ -28,11 +56,11 @@ if %ERRORLEVEL% EQU 0 (
 	for /f "tokens=*" %%i in ('where vcpkg.exe') do set VCPKG_PATH=%%i
 	echo Vcpkg is already installed.
 ) else (
-	if exist "%ProgramFiles(x86)%\vcpkg" (
-		set "VCPKG_PATH=%ProgramFiles(x86)%\vcpkg"
+	if exist "C:\vcpkg" (
+		set "VCPKG_PATH=C:\vcpkg"
 		echo Vcpkg is already installed.
 	) else (
-		set /P "VCPKG_PATH=Enter a path to install vcpkg to (if empty, uses %ProgramFiles(x86)%\vcpkg\): " || set "VCPKG_PATH=%ProgramFiles(x86)%\vcpkg"
+		set /P "VCPKG_PATH=Enter a path to install vcpkg to (Cannot have special characters. If empty, uses C:\vcpkg): " || set "VCPKG_PATH=C:\vcpkg"
 
 		if not exist "!VCPKG_PATH!" (
 			md "!VCPKG_PATH!"
