84bed96d6645857cb8a83269de71379378fd1093
Karim Abdel Hamid <karimah@aucegypt.edu>
2026-05-26 21:28:55 -0400

    C# Scripting / Fix quaternions, add more helper functions

--- plugins/Grindstone.Script.CSharp/CSharpCore/Math/Quaternion.cs
+++ plugins/Grindstone.Script.CSharp/CSharpCore/Math/Quaternion.cs
@@ -3,18 +3,27 @@ using System.Runtime.InteropServices;
 namespace Grindstone {
 	namespace Math {
 		[System.Serializable, StructLayout(LayoutKind.Sequential)]
-		public class Quaternion {
+		public struct Quaternion {
 			#region Public Fields
-			[MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
-			protected float[] array = new float[4];
+			public float x;
+			public float y;
+			public float z;
+			public float w;
 			#endregion
 
 			#region Constructor Methods
 			public Quaternion() {
-				array[0] = 0;
-				array[1] = 0;
-				array[2] = 0;
-				array[3] = 0;
+				x = 0;
+				y = 0;
+				z = 0;
+				w = 1;
+			}
+
+			public Quaternion(float x, float y, float z, float w) {
+				this.x = x;
+				this.y = y;
+				this.z = z;
+				this.w = w;
 			}
 
 			public Quaternion(EulerAngles angles)
@@ -26,15 +35,22 @@ namespace Grindstone {
 				float cp = (float)System.Math.Cos(angles.pitch * 0.5f);
 				float sp = (float)System.Math.Sin(angles.pitch * 0.5f);
 
-				array[0] = sr * cp * cy - cr * sp * sy;
-				array[1] = cr * sp * cy + sr * cp * sy;
-				array[2] = cr * cp * sy - sr * sp * cy;
-				array[3] = cr * cp * cy + sr * sp * sy;
+				x = sr * cp * cy - cr * sp * sy;
+				y = cr * sp * cy + sr * cp * sy;
+				z = cr * cp * sy - sr * sp * cy;
+				w = cr * cp * cy + sr * sp * sy;
 			}
 			#endregion
 
 			#region Public Methods
-			public override string ToString() => $"Quaternion({array[0]}, {array[1]}, {array[2]}, {array[3]})";
+			public static Quaternion Identity => new(0.0f, 0.0f, 0.0f, 1.0f);
+			public readonly EulerAngles ToEulerAngles() => new(this);
+			public override readonly string ToString() => $"Quaternion({x}, {y}, {z}, {w})";
+
+			public static implicit operator string(Quaternion q)
+			{
+				return q.ToString();
+			}
 			#endregion
 		}
 	}
